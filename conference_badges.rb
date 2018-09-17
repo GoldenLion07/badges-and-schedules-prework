2a428c06adaeebfc5970ba7a89f2e18ab441629d
Scott M <gmat191@gmail.com>
2018-09-17 03:56:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def badge_creator(attendees)
+def batch_badge_creator(attendees)
   new_array = []
   array.each do |name|
     new_array.push("Hello, my name is #{name}.")
